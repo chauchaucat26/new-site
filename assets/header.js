a4470636f4f26ba9a4f5aa14f46d303e3b75b2f5
chauchau <tesutoero@gmail.com>
2024-10-16 04:08:55 +0000

    Update header.js

--- assets/header.js
+++ assets/header.js
@@ -1,6 +1,5 @@
 function createheader() {
-    let headdata = '<a href="index.html"><img src="assets/image/favicon.png" class="logo"></a><nav><ul><li><a href="#">ABOUT</a></li><li><a href="#">TEAM</a></li><li><a href="#">NEWS</a></li><li><a href="#">CONTACT</a></li></ul></nav>';
-    let url = "https://math-is-the-best-subject-in-the-world.vercel.app/";
+    let url = "./assets/data/texts/header.txt";
     fetch(url)
     .then(response => {
         if (!response.ok) {
@@ -9,13 +8,11 @@ function createheader() {
         return response.text();
         })
         .then(data => {
-        document.getElementById("header").innerHTML = headdata;
+        document.getElementById("header").innerHTML = data;
         })
         .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
-    
-    console.log(headdata);
 }
 
 createheader()
