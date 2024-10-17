a5b76fa753e25202d61d8fce6c83ca419c4e8219
chauchau <tesutoero@gmail.com>
2024-10-17 01:11:02 +0000

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,22 @@
 function creat() {
     let currentPage = window.location.pathname.split('/').pop();
     let pageName = currentPage.split('.')[0];
-    let cathName = pageName + '-site-data'
-    console.log(pageName)
+    let cathName = pageName + '-site-data';
+    let headurl = './assets/texts/index.txt'
+    fetch(headurl)
+    .then(response => response.text())
+    .then(txtContent => {
+        let lsdata = txtContent
+        let currentTime = new Date().toLocaleString();
+        localStorage.setItem(cathName, lsdata);
+        localStorage.setItem(cathName + 'loadTime', currentTime);
+    })
+    .catch(error => {
+        localStorage.setItem(cathName, error);
+    });
+
+if ('serviceWorker' in navigator) {
+}
 }
 
 creat()
